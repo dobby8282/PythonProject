cf4f2964b55c6094d669cdfa6d8b0a6cc79edd06
ITSC <itsc@google.com>
2023-02-13 21:48:30 +0900

    Day10 html

--- Day09/Section18/Ex18-6-getRnakPage-movie.py
+++ Day09/Section18/Ex18-6-getRnakPage-movie.py
@@ -6,7 +6,8 @@ response = requests.get(url)
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
-movie_list = soup.find_all('div', class_='tit3')
+# movie_list = soup.find_all('div', class_='tit3')
+movie_list = soup.find_all('div', attrs={'class': 'tit3'})
 
 for idx, movie in enumerate(movie_list):
     print('{}위: {}'.format(idx + 1, movie.text.strip()))
