698c22d1f2b2b572d20929cb2aa3b1e345fda986
ITSC <ITSC@test.com>
2023-02-22 21:47:45 +0900

    자료구조

---
+++ Day13/Sort/Ex02-SelectionSort.py
@@ -0,0 +1,14 @@
+'''
+파일명 : Ex02-SelectionSort.py
+    주어진 리스트에서 최소값을 찾아 맨 앞에 있는 값과 교체하는 알고리즘
+    O(n^2)의 시간 복잡도
+'''
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+    return arr
