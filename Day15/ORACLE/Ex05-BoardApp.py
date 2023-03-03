0e3ff72388791a2cf85b5cbdefdafa386d52fc6f
ITSC <ITSC@test.com>
2023-03-03 21:57:30 +0900

    py_board

--- Day15/ORACLE/Ex05-BoardApp.py
+++ Day15/ORACLE/Ex05-BoardApp.py
@@ -10,8 +10,7 @@ CREATE TABLE HR.PY_BOARD (
   BOARD_TITLE      VARCHAR2(255)    DEFAULT NULL         NULL, 
   BOARD_WRITER     VARCHAR2(255)    DEFAULT NULL         NULL, 
   BOARD_CONTENT    CLOB                  NULL, 
-  BOARD_DATE       DATE             DEFAULT SYSDATE                  NULL, 
-  BOARD_TYPE       VARCHAR2(11)      NOT NULL
+  BOARD_DATE       DATE             DEFAULT SYSDATE                  NULL
 );
 
 
